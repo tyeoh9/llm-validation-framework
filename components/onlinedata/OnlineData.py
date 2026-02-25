f66f4aabca8d7784cdd91bdef5ce9deae435cac2
chckn9 <thomasyeoh9@gmail.com>
2026-02-25 12:02:08 -0800

    Make max_results a param

--- components/onlinedata/OnlineData.py
+++ components/onlinedata/OnlineData.py
@@ -1,6 +1,5 @@
 from ddgs import DDGS
 from rank_bm25 import BM25Okapi
-import pprint
 
 '''
 TODO:
@@ -12,8 +11,9 @@ TODO:
 
 class OnlineData():
 
-    def __init__(self):
+    def __init__(self, max_results=10):
         self.searcher = DDGS()
+        self.max_results = max_results
 
     def rank_results(self, claim: str, pages: list[str]) -> list[tuple[str, float]]:
         tokenized_corpus = [p["body"].lower().split() for p in pages]
@@ -23,7 +23,7 @@ class OnlineData():
         return ranked
 
     def search(self, query):
-        results = self.searcher.text(query, max_results=10)
+        results = self.searcher.text(query, max_results=self.max_results)
         top_result = self.rank_results(query, results)[0][0]
         body = top_result["body"]
         href = top_result["href"]
