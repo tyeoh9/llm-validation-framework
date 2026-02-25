64775f0f2e22a9d192335e3cb58a3f524cc44820
chckn9 <thomasyeoh9@gmail.com>
2026-02-25 11:37:00 -0800

    Basic implementation for fetching web data

---
+++ components/onlinedata/OnlineData.py
@@ -0,0 +1,30 @@
+from ddgs import DDGS
+from rank_bm25 import BM25Okapi
+import pprint
+
+'''
+TODO:
+- simplify(): condense a sentence into its essence (used for input + output)
+- rank_results(): Out of the search results, pick the search result with highest BM25 score
+- If there are no good results (<threshold), scrape highest ranked (or maybe popular?) page
+    - Tutorial: https://codesignal.com/learn/courses/navigating-the-web-for-information/lessons/searching-the-web-with-ddgs-in-python
+'''
+
+class OnlineData():
+
+    def __init__(self):
+        self.searcher = DDGS()
+
+    def rank_results(self, claim: str, pages: list[str]) -> list[tuple[str, float]]:
+        tokenized_corpus = [p["body"].lower().split() for p in pages]
+        bm25 = BM25Okapi(tokenized_corpus)
+        scores = bm25.get_scores(claim.lower().split())
+        ranked = sorted(zip(pages, scores), key=lambda x: x[1], reverse=True)
+        return ranked
+
+    def search(self, query):
+        results = self.searcher.text(query, max_results=10)
+        top_result = self.rank_results(query, results)[0][0]
+        body = top_result["body"]
+        href = top_result["href"]
+        return body, href
