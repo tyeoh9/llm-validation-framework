a76817fa565f1597de1b6cbb1a636956d3ad6c3d
chckn9 <thomasyeoh9@gmail.com>
2026-03-04 12:29:13 -0800

    Update LLM-as-a-judge promopt

--- components/accuracy/AccuracyAgent.py
+++ components/accuracy/AccuracyAgent.py
@@ -32,7 +32,6 @@ class AccuracyAgent:
             name="Text Equivalence",
             evaluation_steps=[
                 "Check whether the facts in 'actual output' contradicts any facts in 'expected output'",
-                "You should also heavily penalize omission of detail",
                 "Vague language, or contradicting OPINIONS, are OK",
             ],
             evaluation_params=[
