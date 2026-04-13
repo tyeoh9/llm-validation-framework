96dfbdd7279e9e5ccaa548e20f3e820d1e738a39
LewisLui777 <121061033+LewisLui777@users.noreply.github.com>
2026-04-13 14:36:33 -0700

    Update privacyAgent.py

--- components/PrivacyAgent/privacyAgent.py
+++ components/PrivacyAgent/privacyAgent.py
@@ -13,41 +13,45 @@ if str(ROOT) not in sys.path:
 
 logging.getLogger("transformers").setLevel(logging.ERROR)
 
+import datafog
+import re
+
 class PrivacyAgent:
   def __init__(self):
     pass
-  
+
   #Quick method that cleans away dates and SSIDs quickly
   def preliminary_censor(self,text):
     try:
       cleaned_text = datafog.sanitize(text, engine="regex")
-      return cleaned_text
+      return cleaned_text, cleaned_text != text
     except Exception as e:
       print(f"Error in preliminary_censor: {e}")
-      return text 
-  
+      return text
+
   #More in-depth method that sanitizes names, companies, and more information in addition to dates and SSIDs.
   def in_depth_censor(self,text):
     try:
       cleaned_text = datafog.sanitize(text, engine="smart")
-      return cleaned_text
+      return cleaned_text, cleaned_text != text
     except Exception as e:
       print(f"Error in in_depth_censor: {e}")
       return text
-  
+
   #Custom censor where the client can choose what words or regex expressions to replace as well as their replacements
   def custom_regex_censor(self,text,bad_words = [],replacements = []):
     if len(bad_words) != len(replacements):
       raise ValueError("bad_words and replacements must be the same length in custom_regex_censor")
-    
+
+    cleaned_text = text
     for bad_word, replacement in zip(bad_words,replacements):
-      text = re.sub(bad_word,replacement,text,flags=re.IGNORECASE)
-    
-    return text
-  
+      cleaned_text = re.sub(bad_word,replacement,cleaned_text,flags=re.IGNORECASE)
+
+    return cleaned_text, cleaned_text != text
+
   #Combines the top 3 into one method for ease of use
   def complete_censor(self,text,bad_words=[],replacements=[]):
-    text = self.preliminary_censor(text)
-    text = self.in_depth_censor(text)
-    text = self.custom_regex_censor(text,bad_words,replacements)
-    return text
+    text, A = self.preliminary_censor(text)
+    text, B = self.in_depth_censor(text)
+    text, C = self.custom_regex_censor(text,bad_words,replacements)
+    return text, A or B or C
