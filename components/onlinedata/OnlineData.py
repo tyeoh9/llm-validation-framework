11d2f1e7a57f76d1c61a4e8d996f246c55d7979d
chckn9 <thomasyeoh9@gmail.com>
2026-04-14 09:37:31 -0700

    Implement logic to condense search queries

--- components/onlinedata/OnlineData.py
+++ components/onlinedata/OnlineData.py
@@ -3,8 +3,6 @@ from rank_bm25 import BM25Okapi
 
 '''
 TODO:
-- simplify(): condense a sentence into its essence (used for input + output)
-    - OR we can just find the text snippet which best matches the query
 - If there are no good results (<threshold), scrape highest ranked (or maybe popular?) page
     - Tutorial: https://codesignal.com/learn/courses/navigating-the-web-for-information/lessons/searching-the-web-with-ddgs-in-python
 - Find a way to make sure the facts are accurate
@@ -24,8 +22,26 @@ class OnlineData:
         ranked = sorted(zip(pages, scores), key=lambda x: x[1], reverse=True)
         return ranked
 
+    def simplify(self, query: str, max_words: int = 10) -> str:
+        """Condense a query to its key terms by stripping stop words."""
+        stop_words = {
+            "a", "an", "the", "is", "are", "was", "were", "be", "been", "being",
+            "have", "has", "had", "do", "does", "did", "will", "would", "could",
+            "should", "may", "might", "shall", "can", "to", "of", "in", "on",
+            "at", "by", "for", "with", "about", "into", "from", "up", "down",
+            "it", "its", "this", "that", "these", "those", "i", "me", "my",
+            "we", "our", "you", "your", "he", "him", "his", "she", "her",
+            "they", "them", "their", "and", "but", "or", "nor", "so", "yet",
+            "as", "if", "when", "while", "which", "who", "what", "how",
+            "also", "very", "just", "there", "then", "than", "not", "only",
+        }
+        words = query.split()
+        key_words = [w for w in words if w.lower().strip(".,!?;:\"'") not in stop_words]
+        return " ".join(key_words[:max_words])
+
     def search(self, query):
-        results = self.searcher.text(query, max_results=self.max_results)
+        search_query = self.simplify(query)
+        results = self.searcher.text(search_query, max_results=self.max_results)
         top_result = self.rank_results(query, results)[0][0]
         body = top_result["body"]
         href = top_result["href"]
